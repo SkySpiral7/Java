90687130f107df6db9b50e8d08f4246c2b6d252e
Ryan Worcester <rworcest@g.emporia.edu>
2017-07-13 20:52:07 -0500

    Moved out Static Serialization

--- src/main/java/com/github/SkySpiral7/Java/serialization/StaticSerializable.java
+++
@@ -1,7 +0,0 @@
-package com.github.SkySpiral7.Java.serialization;
-
-public interface StaticSerializable
-{
-   //public static T<this> readFromStream(final ObjectStreamReader reader)
-   public void writeToStream(final ObjectStreamWriter writer);
-}
